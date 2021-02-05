7eb12d5cd81561f7e7be1418a09dfc7dd4908349
Emily <emnagai89@gmail.com>
2021-02-04 22:04:11 -0800

    commented out json.decoder code because it wasn't getting t hrough the data structure

--- mindfull-front-end/mindfull-front-end/FoodDetail.swift
+++ mindfull-front-end/mindfull-front-end/FoodDetail.swift
@@ -76,22 +76,70 @@ class FoodDetail: FoodData {
         
         //get data wth .dataTask method
         let task = session.dataTask(with: url) { (data, response, error) in
+            let jsonDecoder = JSONDecoder()
             if let error = error {
-                print("Error: \(error.localizedDescription)")
+                print(error)
+                completed()
+                return
             }
-            //deal with the data
+            guard let data = data else {
+                print("data was nil")
+                completed()
+                return
+            }
             do {
-//                let json = try JSONSerialization.jsonObject(with: data!, options: [])
-                let result = try JSONDecoder().decode(Result.self, from: data!)
-//                print(" \(result)")
-//                print("The Result is: \(result.calories)")
-                self.text = result.ingredients[0].text
+                print("IS THIS WORKING?")
+                let resultStruct = try jsonDecoder.decode(Result.self, from: data)
+                print("\(resultStruct.ingredients[0].text)")
+                self.name = resultStruct.ingredients[0].text
+                print(self.text)
+                print("\(resultStruct.ingredients[0].parsed[0].nutrients.CHOCDF.label)")
+                print("\(resultStruct.ingredients[0].parsed[0].nutrients.PROCNT.label)")
+                print("\(resultStruct.ingredients[0].parsed[0].nutrients.FAT.label)")
+                completed()
+
             } catch {
-                print("JSON Error: \(error.localizedDescription)")
+                print(error)
+                completed()
             }
-            completed()
         }
-        
         task.resume()
+//            if let error = error {
+//                print("Error: \(error.localizedDescription)")
+//            }
+//            //deal with the data
+//            do {
+//                print("Data: \(data!)")
+////                let json = try JSONSerialization.jsonObject(with: data!, options: [])
+//                let result = try JSONDecoder().decode(Result.self, from: data!)
+////                print("This is the result: \(result)")
+////                print(" \(result)")
+////                print("The Result is: \(result.calories)")
+////                print("Result = \(result[0])")
+////               var resultType = type(of: result)
+////                print(resultType)
+////                self.text = result.ingredients[0].text
+////                self.fatLabel = result.ingredients[0].parsed[0].nutrients.FAT.label
+////                self.carbLabel = result.ingredients[0].parsed[0].nutrients.CHOCDF.label
+////                self.proteinLabel = result.ingredients[0].parsed[0].nutrients.PROCNT.label
+//
+////            }
+////            catch {
+////                print("JSON Error: \(error.localizedDescription)")
+//            } catch DecodingError.keyNotFound(let key, let context) {
+//                Swift.print("could not find key \(key) in JSON: \(context.debugDescription)")
+//            } catch DecodingError.valueNotFound(let type, let context) {
+//                Swift.print("could not find type \(type) in JSON: \(context.debugDescription)")
+//            } catch DecodingError.typeMismatch(let type, let context) {
+//                Swift.print("type mismatch for type \(type) in JSON: \(context.debugDescription)")
+//            } catch DecodingError.dataCorrupted(let context) {
+//                Swift.print("data found to be corrupted in JSON: \(context.debugDescription)")
+//            } catch let error as NSError {
+//                NSLog("Error in read(from:ofType:) domain= \(error.domain), description= \(error.localizedDescription)")
+//            }
+//            completed()
+//        }
+//
+//        task.resume()
     }
 }
