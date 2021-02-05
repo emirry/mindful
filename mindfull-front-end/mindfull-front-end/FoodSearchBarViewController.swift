396213e75e2c26fc20ff32e2567d1206f6134f0a
Emily <emnagai89@gmail.com>
2021-02-05 13:39:33 -0800

    created foodSearchBarVC and connected searh bar outlet

---
+++ mindfull-front-end/mindfull-front-end/FoodSearchBarViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FoodSearchBarViewController.swift
+//  mindfull-front-end
+//
+//  Created by Emily Nagai on 2/5/21.
+//
+
+import UIKit
+
+class FoodSearchBarViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
