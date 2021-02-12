1942e67fa25f0c730c12a793085c9bd508a240c3
Emily <emnagai89@gmail.com>
2021-02-12 00:36:30 -0800

    weightloss calculation methods

--- app/foodjournal/models.py
+++ app/foodjournal/models.py
@@ -8,6 +8,58 @@ class User(models.Model):
     height = models.IntegerField(blank=False)
     age = models.IntegerField(blank=False)
 
+    def bmr_calculations(maleOrFemale, weight, height, age):
+        #Basal Metabolic Rate
+        weightInKgs = weight / 2.2
+        heightInCent = height * 2.54
+        if maleOrFemale == "M":
+            bmr = int((10 * weightInKgs) + (6.25 * heightInCent) - (5 * age) + 5)
+        elif maleOrFemale == "F":
+            bmr = int((10 * weightInKgs) + (6.25 * heightInCent) - (5 * age) - 161)
+        return bmr
+
+    def daily_caloric_needs(bmr, activityLevel):
+        """
+            1 = Sedentary
+            2 = Exercise 1 - 3 times a week
+            3 = Exercise 4 - 5 times a week
+            4 = Daily Exercise or intense exercise 3-4 times a week
+            5 = Intense Exercise 6 times a week
+        """
+
+        if activityLevel == 1:
+            levelIndex = 1.2
+        elif activityLevel == 2:
+            levelIndex = 1.375
+        elif activityLevel == 3:
+            levelIndex = 1.46
+        elif activityLevel == 4:
+            levelIndex = 1.725
+        elif activityLevel == 5:
+            levelIndex = 1.9
+        
+        #Recommended caloric intake to maintain weight
+        dailyCalIntake = int(bmr * levelIndex)
+        return dailyCalIntake
+
+    def calculate_macros(calories):
+        #calories = dailyCalIntake
+        #Calculate maintenance macros 
+        cal_from_protein = int(.4 * calories)
+        grams_of_protein = int(cal_from_protein / 4)
+        cal_from_carbs = int(.4 * calories)
+        grams_of_carbs = int(cal_from_protein / 4)
+        cal_from_fat = int(.2 * calories)
+        grams_of_fat = int(cal_from_fat / 9)
+
+        #based on caloric intake, this calculates caloric intake of P/C/F and grams
+
+    #INFO TO RETURN:
+    bmr = bmr_calculations(maleOrFemale, weight, height, age)
+    dailyCalIntake = daily_caloric_needs(bmr)
+    calculate_macros(dailyCalIntake)
+
+
 class FoodJournal(models.Model):
     date = models.DateField(auto_now=True, blank=False, null=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
