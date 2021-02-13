9075c7e282d4e7fc7b54c2d0f22968c6155d5324
Emily <emnagai89@gmail.com>
2021-02-12 17:58:23 -0800

    impemented UserResponse class for post request

--- mindfull-front-end/mindfull-front-end/UserDetail.swift
+++ mindfull-front-end/mindfull-front-end/UserDetail.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+class UserDetail: Codable {
+    var user_name:String
+    var maleOrFemale: String
+    var weight: Int
+    var height: Int
+    var age: Int
+    var activityLevel: Int
+    
+    init(user_name: String, maleOrFemale: String, weight: Int, height: Int, age: Int, activityLevel: Int) {
+        self.user_name = user_name
+        self.maleOrFemale = maleOrFemale
+        self.weight = weight
+        self.height = height
+        self.age = age
+        self.activityLevel = activityLevel
+    }
+    
+}
+
+class UserResponse: Codable {
+    //REVIEW: might have include more variables
+    var bmr: Int
+    var calToMaintain: Int
+    var calToLose: Int
+    var macros: Int
+}
