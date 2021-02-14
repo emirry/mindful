5915b50c15b380626d9b64da34862877a7b8b959
Emily <emnagai89@gmail.com>
2021-02-14 15:22:19 -0800

    google button functions

--- mindfull-front-end/mindfull-front-end/SignInViewController.swift
+++ mindfull-front-end/mindfull-front-end/SignInViewController.swift
@@ -6,17 +6,44 @@
 //
 
 import UIKit
+import GoogleSignIn
 
-class SignInViewController: UIViewController {
+class SignInViewController: UIViewController, GIDSignInDelegate {
 
+
+    @IBOutlet weak var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        GIDSignIn.sharedInstance()?.delegate = self
+          // Automatically sign in the user.
+//          GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+        
+    }
+    
+    @IBAction func login(_ sender: UIButton) {
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance()?.signIn()
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if error != nil {
+            print(user.userID!)
+        }
+
 
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        <#code#>
-//    }
 
+    //Sign out
+//    @objc func signOutButtonTapped(_ sender: UIButton) {
+//        GIDSignIn.sharedInstance()?.signOut()
+//    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+
+
+        }
     
 }
