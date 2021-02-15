42ae3a6efc2414d3e4c261e130963a13299c2cbf
Emily <emnagai89@gmail.com>
2021-02-15 10:08:21 -0800

    changed nav title in welcome screen, changed bundle id, and hooked up app to phone

--- mindfull-front-end/mindfull-front-end/WelcomeViewController.swift
+++ mindfull-front-end/mindfull-front-end/WelcomeViewController.swift
@@ -14,22 +14,11 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "Welcome to mindfull!"
+        self.navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont(name: "Thasadith-Regular", size: 20)!]
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 246/225.0, green: 141/225.0, blue: 95/225.0, alpha: 1)
         self.navigationItem.setHidesBackButton(true, animated: true)
 
-//        self.navigationController?.navigationBar.barTintColor = UIColor(red: 246/225.0, green: 141/225.0, blue: 95/225.0, alpha: 1)
-
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
